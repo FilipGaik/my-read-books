a25862a7711fde7915865e0e5a7e685ff0e77605
FilipGaik <126327646+FilipGaik@users.noreply.github.com>
2024-02-13 19:39:08 +0100

    Registration module added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import RegistrationLoginMenu from "./RegistrationLoginMenu";
+import Books from "./Books";
+import axios from "axios";
 
 function App() {
-  let isAuthenticated = false;
+  const [loggedUser, setLoggedUser] = useState("");
+
+  async function handleRegister(user) {
+    if(user.email === "" || user.password === "") {
+      alert("Please fulfill both fields.");
+    } else {
+      try {
+        const response = await axios.post("http://localhost:5000/register", user, {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          }
+        });
+
+        if(response.data === "OK") {
+          setLoggedUser(user.email);
+        } else {
+          alert("Email already exists. Try logging in.");
+        }
+      } catch(error) {
+        console.log(error);
+      }
+    }
+  }
+
+  function handleLogout() {
+    setLoggedUser("");
+  }
 
   return (
     <div>
       <Header />
-      {!isAuthenticated && <RegistrationLoginMenu />}
+      {loggedUser === "" && <RegistrationLoginMenu handleRegister={handleRegister} />}
+      {loggedUser !== "" && <Books handleLogout={handleLogout} />}
       <Footer />
     </div>
   );
