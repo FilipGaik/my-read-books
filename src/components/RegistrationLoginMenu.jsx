8a1336dec496a075cac68e6f101fcb250891d848
FilipGaik <126327646+FilipGaik@users.noreply.github.com>
2024-02-13 19:40:31 +0100

    Registration module added

--- src/components/RegistrationLoginMenu.jsx
+++ src/components/RegistrationLoginMenu.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function RegistrationLoginMenu() {
+export default function RegistrationLoginMenu(props) {
+  const [user, setUser] = useState({email: "", password: ""});
+
+  function changeHandler(e) {
+    const {name, value} = e.target;
+
+    setUser((previousValue) => ({...previousValue, [name]: value}));
+  }
+
   return (
     <div className="RegLogMenu">
       <p>Do you <strong>love</strong> reading <strong>books</strong>?</p>
       <p>Use this <strong>site</strong> to wright down your <strong>thoughts</strong> about books you have read.</p>
       <p>Register for <strong>free</strong> or login if you have an account already.</p>
+
       <form>
-        <input type="email" placeholder="Email"></input>
+        <input type="email" name="email" placeholder="Email" required autoFocus autoComplete="on" onChange={changeHandler} value={user.email}></input>
         <br />
-        <input type="password" placeholder="Password"></input>
+        <input type="password" name="password" placeholder="Password" required onChange={changeHandler} value={user.password}></input>
         <br />
-        <button type="submit" id="login">Log in</button>
+        <button type="submit" className="login">Log in</button>
         <br />
-        <button type="submit" id="register">Register</button>
+        <button type="submit" onClick={(e) => {
+          props.handleRegister(user);
+          e.preventDefault();
+        }} className="register">Register</button>
       </form>
     </div>
   );
