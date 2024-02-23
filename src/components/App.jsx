b77f3afdd7c6edac86189febdbf66861d7fe2a85
FilipGaik <126327646+FilipGaik@users.noreply.github.com>
2024-02-23 23:21:29 +0100

    Adding books module added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import RegistrationLoginMenu from "./RegistrationLoginMenu";
-import Books from "./Books";
+import Logout from "./Logout";
+import Book from "./Book";
+import Form from "./Form";
 import axios from "axios";
 
-function App() {
+export default function App() {
   const [loggedUser, setLoggedUser] = useState("");
+  const [books, setBooks] = useState([]);
 
   async function handleLogin(user) {
     if(user.email === "" || user.password === "") {
@@ -25,6 +28,7 @@ function App() {
           alert("Wrong password, please try again.");
         } else {
           setLoggedUser(user.email);
+          getBooks(user.email);
         }
       } catch(error) {
         console.log(error);
@@ -58,14 +62,47 @@ function App() {
     setLoggedUser("");
   }
 
+  async function addNewBook(book) {
+    if(book.title === "" || book.author === "" || book.description === "") {
+      alert("Please fulfill all fields.");
+    } else {
+      try {
+        const response = await axios.post("http://localhost:5000/addBook", {title: book.title, author: book.author, description: book.description, email: loggedUser}, {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          }
+        });
+        if(response.data === "NOK") {
+          alert("Something went wrong, please try again.");
+        } else {
+          alert("Book added!");
+          setBooks((previousValue) => ([...previousValue, {title: book.title, author: book.author, description: book.description, id: response.data.id}]));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  async function getBooks(email) {
+    try {
+      const response = await axios.post("http://localhost:5000/getBooks", {email: email}, {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          }
+        });
+      setBooks([...response.data]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div>
       <Header />
       {loggedUser === "" && <RegistrationLoginMenu handleRegister={handleRegister} handleLogin={handleLogin} />}
-      {loggedUser !== "" && <Books handleLogout={handleLogout} />}
+      {loggedUser !== "" && <div><Logout handleLogout={handleLogout} />{books.map(book => <Book key={book.id} title={book.title} author={book.author} description={book.description} id={book.id} />)}<Form addNewBook={addNewBook} /></div>}
       <Footer />
     </div>
   );
 }
-
-export default App;
