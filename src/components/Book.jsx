7cdaef7e947bde43b236cb2c49ac87378567ee7f
FilipGaik <126327646+FilipGaik@users.noreply.github.com>
2024-02-25 00:31:43 +0100

    Edit description module added

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -4,8 +4,26 @@ export default function Book(props) {
   return (
     <div className="RegLogMenu">
       <h4>{props.title} by {props.author}</h4>
-      <p>{props.description}</p>
-      <button id={props.id} className="login">Edit</button>
+      <p id={"paragraph" + props.id}>{props.description}</p>
+      <textarea rows="8" cols="50" id={"input" + props.id} type="text" autoComplete="off" autoFocus={true} hidden={true} onChange={props.updateDescription} />
+      <br id={"br" + props.id} hidden={true} />
+      <button id={"edit" + props.id} className="login" onClick={(e) => {
+          document.getElementById("paragraph" + props.id).setAttribute("hidden", true);
+          document.getElementById("edit" + props.id).setAttribute("hidden", true);
+          document.getElementById("input" + props.id).removeAttribute("hidden");
+          document.getElementById("br" + props.id).removeAttribute("hidden");
+          document.getElementById("save" + props.id).removeAttribute("hidden");
+
+          const description = document.getElementById("paragraph" + props.id).innerText;
+          
+          document.getElementById("input" + props.id).value = description;
+          props.setUpdatedDescription(description);
+          e.preventDefault();
+        }}>Edit</button>
+      <button id={"save" + props.id} className="login" hidden={true} onClick={(e) => {
+          props.saveUpdatedDescription(e);
+          e.preventDefault();
+        }}>Save</button>
     </div>
   );
 }
